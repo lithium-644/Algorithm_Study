564771d08d39b1f41d93634576c795eb735736cd
lithium-644 <ananec.dot@gmail.com>
2023-01-12 20:22:07 +0900

    [8주차] 백준 CLASS2 에센셜 풀이

---
+++ 신진영/[8주차] 백준 CLASS2 에센셜/10250_ACM 호텔.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    h, w, n = map(int, input().split())
+    
+    a = n % h
+    b = n // h+1
+    
+    if a == 0:
+        a = h
+        b -= 1
+    print(a*100 + b)
